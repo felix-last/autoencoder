599b453a97f5d7645ea4e0107f2e014f92866620
Felix Last <mail@felixlast.de>
2016-11-30 13:02:34 +1300

    Avoid empty clusters and add q_msse_threshold

--- autoencoder.py
+++ autoencoder.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import sklearn.utils
 
 class Autoencoder:
-    def __init__(self, layers=[3,2,3], training_set=None, activation_fn=T.nnet.sigmoid):
+    def __init__(self, layers, training_set, activation_fn=T.nnet.sigmoid):
         self.layers = layers
         self.layer_count = len(layers)
         self.features_count = layers[0]
@@ -106,27 +106,27 @@ class Autoencoder:
             if self.clustering:
                 current['MSSE'] = np.asscalar(function([self.X],self.MSSE)(self.training_set))
                 current['Clustering Cost'] = np.asscalar(function([self.X],self.clustering_cost)(self.training_set))
-                if self.q.get_value() != 1:
-                    current['Clustering Cost * q'] = np.asscalar(function([self.X],self.q*self.clustering_cost)(self.training_set))
+                current['Clustering Cost * q'] = np.asscalar(function([self.X],self.q*self.clustering_cost)(self.training_set))
             self.stats.append(current)
 
     def get_stats(self, as_df=False):
         if as_df:
             df_stats = pd.DataFrame(self.stats)
             if len(self.stats) > 0:
-                df_stats.set_index('epoch')
-                df_stats.index.set_names(['epoch'], inplace=True)
-                df_stats.drop('epoch', axis=1, inplace=True)
+                df_stats.set_index('epoch',inplace=True)
             return df_stats
         else:
             return self.stats
 
-    def plot_stats(self):
+    def plot_stats(self, stacked=False):
         stats = self.get_stats(as_df=True)
         if len(stats) > 1:
-            stats.plot()
+            if stacked:
+                stats[['MSSE', 'Clustering Cost * q']].plot.area()
+            else:
+                return stats.plot()
         else:
-            print('Nothing to plot.')
+            print('Nothing to plot')
 
     def adjust_eta(self, eta_strategy, epoch):
         self.eta.set_value(
@@ -149,12 +149,13 @@ class Autoencoder:
             self.adjust_eta(eta_strategy, epoch)
 
     def prepare_clustering(self, data, k, q):
-        self.initialize_clusters_and_assignment(data, k, q)
         self.clustering = True
         X = self.X
         A = self.A
         
         H = self.H = A[self.layer_count // 2] # middle hidden layer, mapping function
+
+        self.initialize_clusters_and_assignment(data, k, q)
         C = self.C # cluster centroids on H
         
         # C_closest_index = self.C_closest_index = T.argmin(((C-H)**2).sum(axis=1)) # index of closest cluster centroid to a given observation
@@ -175,11 +176,20 @@ class Autoencoder:
                 non_sequences=[self.distance_closest_cluster,self.current_cluster_assignment]
             )
         self.clustering_cost = self.per_cluster_cost.sum()
+
+        def calculate_cluster_centroid(k, H, assignment):
+            assigned_observations = (H[T.eq(assignment, T.fill(assignment,k)).nonzero()])
+            return theano.ifelse.ifelse(
+                T.neq(assigned_observations.size, 0), # if there are assigned_observations
+                assigned_observations.mean(axis=0), # then return their mean,
+                H[k] # else use the k-th observation as the centroid (assuming data is shuffled)
+            )
         self.cluster_centroids, _ = theano.scan(
-            fn=lambda k, H, assignment: (H[T.eq(assignment, T.fill(assignment,k)).nonzero()]).mean(axis=0),
+            fn=calculate_cluster_centroid,#lambda k, H, assignment: (H[T.eq(assignment, T.fill(assignment,k)).nonzero()]).mean(axis=0),
             sequences=[T.arange(C.shape[0])],
             non_sequences=[H,self.current_cluster_assignment]
         )
+        
         self.update_cluster_centroids(data)
         self.update_cluster_assignment(data)
 
@@ -199,16 +209,18 @@ class Autoencoder:
     def update_cluster_centroids(self,data):
         self.C.set_value( function([self.X], self.cluster_centroids)(data) )
 
-    def cluster(self, epochs, learning_rate, q, k, minibatch_size, eta_strategy=(lambda eta, epoch: eta), collect_stats_every_nth_epoch=1, plot_clusters_every_nth_epoch=1):
+    def cluster(self, epochs, learning_rate, q, k, minibatch_size, q_msse_threshold=-1, eta_strategy=(lambda eta, epoch: eta), collect_stats_every_nth_epoch=1, plot_clusters_every_nth_epoch=1):
         self.eta.set_value(learning_rate)
         if minibatch_size < 1:
             minibatch_size = minibatch_size * self.training_set.shape[0]
 
         clustering_sample = sklearn.utils.shuffle( # use a random sample of ..% for clustering  
             self.training_set, 
-            n_samples=(self.training_set.shape[0]//4)
+            n_samples=minibatch_size#(self.training_set.shape[0]//4)
         )
         self.prepare_clustering(data=clustering_sample, k=k, q=q)
+        if q_msse_threshold > 0:
+            self.q.set_value(0)
         self.cost = self.MSSE + (self.q * self.clustering_cost)    
 
         self.collect_stats(0)
@@ -219,10 +231,24 @@ class Autoencoder:
             if (epoch % collect_stats_every_nth_epoch) == 0:
                 self.collect_stats(epoch)
             self.adjust_eta(eta_strategy, epoch)
-            self.update_cluster_centroids(clustering_sample)
-            self.update_cluster_assignment(clustering_sample)
+            if(self.q.get_value() != 0):
+                self.update_cluster_centroids(clustering_sample)
+                self.update_cluster_assignment(clustering_sample)
             if epoch == 1 or (epoch % plot_clusters_every_nth_epoch) == 0:
                 self.plot_clusters(clustering_sample, epoch)
+            
+            # debug
+            
+
+            if q_msse_threshold > 0 and self.q.get_value() == 0:
+                if epoch % 10 == 0:
+                    msse = function([self.X],self.MSSE)(self.training_set)
+                    if msse < q_msse_threshold:
+                        print('Epoch', epoch, ': MSSE', msse, 'is now smaller than Q-MSSE-Threshold', q_msse_threshold)
+                        print('Beginning clustering.')
+                        self.q.set_value(q)
+                        self.update_cluster_assignment(clustering_sample)
+                        self.update_cluster_centroids(clustering_sample)
         self.update_cluster_assignment(self.training_set)
         self.update_cluster_centroids(self.training_set)
         self.plot_clusters(self.training_set, 'Final (entire set)')
@@ -230,20 +256,31 @@ class Autoencoder:
 
     def plot_clusters(self, data, epoch):
         np_H = theano.function([self.X],self.H)(data)
-        np_assignment = function([self.X], self.cluster_assignment)(data)
-        np_cluster_centroids = function([self.X], self.cluster_centroids)(data)
-
+        np_assignment = self.current_cluster_assignment.get_value()#function([self.X], self.cluster_assignment)(data)
+        np_cluster_centroids = self.C.get_value()#function([self.X], self.cluster_centroids)(data)
         colors = ['b','r','g','c','m','y','k','w']
+        if np_cluster_centroids.shape[0] > len(colors):
+            colors = ['#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9','#bc80bd','#ccebc5','#ffed6f'] + colors
         plt.figure()
+        #plt.axis([0,1,0,1])
+        plt.axis('equal')
+        if np_H.shape[1] < 2:
+            y = np.ones_like(np_H)
+        else:
+            y = np_H[:,1]
         plt.scatter(
             x=np_H[:,0], 
-            y=np_H[:,1], 
-            marker='x', 
+            y=y, 
+            marker='+', 
             color=[ colors[i] for i in np_assignment ]
         )
+        if np_cluster_centroids.shape[1] < 2:
+            y = np.ones_like(np_cluster_centroids)
+        else:
+            y = np_cluster_centroids[:,1]
         plt.scatter(
             x=np_cluster_centroids[:,0],
-            y=np_cluster_centroids[:,1],
+            y=y,
             marker='o',
             color=colors
         )
